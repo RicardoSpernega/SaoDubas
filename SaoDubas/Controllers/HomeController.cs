ca05f5f9fa2457c9d0be638d22bb66d6dc4107d7
ricardo rollo <ricardo_srd@hotmail.com>
2020-08-12 11:34:21 -0300

    First Commit

---
+++ SaoDubas/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SaoDubas.Domain.Interfaces.Repositories;
+using SaoDubas.Models;
+
+namespace SaoDubas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public HomeController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var prod = _produtoRepository.ListarProdutos();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
